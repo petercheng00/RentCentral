85d9578c0d7a75d38cf61c786575c680d0d4c895
Peter Cheng <petercheng00@gmail.com>
2012-04-04 15:57:49 -0700

    renamed boolean

--- db/migrate/20120404204205_add_public_to_listings.rb
+++ db/migrate/20120404204205_add_public_to_listings.rb
@@ -1,7 +1,7 @@
 class AddPublicToListings < ActiveRecord::Migration
   def up
-    add_column :listings, :is_active, :boolean, :default => 1
-    add_column :listings, :is_public, :boolean, :default => 1
+    add_column :listings, :is_active, :boolean, :default => true
+    add_column :listings, :is_public, :boolean, :default => true
   end
 
   def down
